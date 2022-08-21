cbba3e03bec9d4a0efba5456baced96202322732
mxzawadzki <srebrniki@o2.pl>
2022-08-21 23:07:22 +0200

    Lint

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
 </template>
 
 <script lang="ts">
-import Header from '@/components/Header/Header.vue';
-import Menu from '@/components/Menu/index.vue';
+import Header from '@/components/Header/Header.vue'
+import Menu from '@/components/Menu/index.vue'
 export default {
   name: 'App',
   components: {
     Header,
-    Menu,
-  },
-};
+    Menu
+  }
+}
 </script>
