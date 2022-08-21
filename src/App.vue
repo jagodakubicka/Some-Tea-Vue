7f371993087a36fa38291971696d447149894b36
mxzawadzki <srebrniki@o2.pl>
2022-08-21 23:04:37 +0200

    Rename to index

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <Header></Header>
-  <Menu></Menu>
+  <Menu />
 </template>
 
 <script lang="ts">
 import Header from '@/components/Header/Header.vue';
-import Menu from '@/components/Menu/Menu.vue';
+import Menu from '@/components/Menu/index.vue';
 export default {
   name: 'App',
   components: {
